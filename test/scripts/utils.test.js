501708c7c78d0662a3fdc334800b5fbc38a6346b
Qiyun Dai <pianoquin@gmail.com>
2024-03-20 08:56:28 -0500

    fixed the UT

--- test/scripts/utils.test.js
+++ test/scripts/utils.test.js
@@ -3,12 +3,20 @@ import { expect } from '@esm-bundle/chai';
 import { decorateArea } from '../../scripts/utils.js';
 
 document.head.innerHTML = await readFile({ path: './mocks/head.html' });
-document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+const marqueeMain = await readFile({ path: './mocks/body-with-marquee.html' });
+const heroMain = await readFile({ path: './mocks/body-without-marquee.html' });
 
-describe('Decorating', () => {
+describe('Decorating LCP', () => {
   it('with marquee', () => {
+    document.body.innerHTML = marqueeMain;
     decorateArea(document.querySelector('main'));
-    expect(true);
-    // placeholder
+    console.log(document.body.querySelector('img')?.loading);
+    expect(document.body.querySelector('img').getAttribute('loading')).to.equal(null);
+  });
+
+  it('without marquee', () => {
+    document.body.innerHTML = heroMain;
+    decorateArea(document.querySelector('main'));
+    expect(document.body.querySelector('img').getAttribute('loading')).to.equal(null);
   });
 });
