62bcd995e79a4a0a314a3dcce174eca005d08d1b
Qiyun Dai <pianoquin@gmail.com>
2024-04-22 10:20:25 -0500

    handler refactor with controllers

--- blocks/form-handler/form-handler.js
+++ blocks/form-handler/form-handler.js
@@ -2,56 +2,22 @@ import { getLibs } from '../../scripts/utils.js';
 
 const { createTag } = await import(`${getLibs()}/utils/utils.js`);
 
-// mapping block names to their init callbacks
-const COMPONENT_CB_MAP = {
-  'checkbox-component': (component) => {
-    const checkboxes = component.querySelectorAll('input[type="checkbox"]');
-    const minReg = component.className.match(/min-(.*?)( |$)/);
-    const maxReg = component.className.match(/max-(.*?)( |$)/);
-    const required = !!minReg;
-    const min = minReg ? parseInt(minReg[1], 10) : 0;
-    const max = maxReg ? parseInt(maxReg[1], 10) : 0;
-
-    const configs = {
-      required,
-      min,
-      max,
-    };
-
-    let boxesChecked = 0;
-    checkboxes.forEach((cb) => {
-      cb.addEventListener('change', () => {
-        if (cb.checked) {
-          boxesChecked += 1;
-        } else {
-          boxesChecked -= 1;
-        }
-
-        checkboxes.forEach((c) => {
-          c.required = boxesChecked < configs.min;
-        });
-
-        if (boxesChecked === configs.max) {
-          checkboxes.forEach((c) => {
-            if (!c.checked) c.disabled = true;
-          });
-        } else {
-          checkboxes.forEach((c) => {
-            c.disabled = false;
-          });
-        }
-      });
-    });
-  },
-};
+// list of controllers for the hanler to load
+const SUPPORTED_COMPONENTS = [
+  'checkbox',
+  // 'event-info',
+  // 'img-upload',
+  // 'venu-info',
+];
 
 function initComponents(el) {
-  Object.entries(COMPONENT_CB_MAP).forEach(([blockName, cb]) => {
-    const mappedComponents = el.querySelectorAll(`.${blockName}`);
+  SUPPORTED_COMPONENTS.forEach((comp) => {
+    const mappedComponents = el.querySelectorAll(`.${comp}-component`);
     if (!mappedComponents?.length) return;
 
-    mappedComponents.forEach((component) => {
-      cb(component);
+    mappedComponents.forEach(async (component) => {
+      const { default: initComponent } = await import(`./controllers/${comp}-component-controller.js`);
+      initComponent(component);
     });
   });
 }
