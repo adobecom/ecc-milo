43cfdec18fef5e5d6cecfd6ca80712f53a9b9e40
rea01581 <rea01581@adobe.com>
2024-05-13 13:39:35 -0700

    event agenda component

---
+++ blocks/form-handler/controllers/event-agenda-component-controller.js
@@ -0,0 +1,20 @@
+
+
+
+export function onSubmit(component, inputMap) {
+  //Todo agenda details 
+  const agendaDetails = component.querySelector('#textfield > input');
+ 
+  const time = component.querySelector('#time-picker-time').value;
+  const agendaInfoVisible = component.querySelector('#checkbox-agenda-info-will-appear-post-event').checked;
+
+  
+
+  const eventInfo = {
+    ...getMappedInputsOutput(component, inputMap),
+    agendaInfoVisible,
+    'Time': time,
+  };
+
+  return eventInfo;
+}
