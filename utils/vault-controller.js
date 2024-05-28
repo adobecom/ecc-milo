f6c2f6b2c993f4bdb5b1388ef14bd7c6bbe0f450
Qiyun Dai <pianoquin@gmail.com>
2024-05-28 13:09:39 -0500

    Create vault-controller.js

---
+++ utils/vault-controller.js
@@ -0,0 +1,23 @@
+async function getVaultToken(roleId, secretId) {
+  const url = 'http://127.0.0.1:8200/v1/auth/approle/login';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      role_id: roleId,
+      secret_id: secretId,
+    }),
+  });
+  const data = await response.json();
+  return data.auth.client_token;
+}
+
+async function getSecret(token, secretPath) {
+  const url = `http://127.0.0.1:8200/v1/${secretPath}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: { 'X-Vault-Token': token },
+  });
+  const data = await response.json();
+  return data.data.data;
+}
