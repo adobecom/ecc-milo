631318fe4b2265fd2ecdebdcf6bd43ab832c09f8
Qiyun Dai <pianoquin@gmail.com>
2024-05-30 18:12:51 -0500

    partners visible data stored as well

--- blocks/form-handler/controllers/event-partners-component-controller.js
+++ blocks/form-handler/controllers/event-partners-component-controller.js
@@ -1,9 +1,10 @@
 export function onSubmit(component, props) {
   const partnerSelectors = component.querySelectorAll('partner-selector');
+  const partnerVisible = component.querySelector('#partners-visible')?.checked;
 
   const partners = Array.from(partnerSelectors).map((p) => p.getSelectedPartner());
 
-  props.payload = { ...props.payload, partners };
+  props.payload = { ...props.payload, partners, partnerVisible };
 }
 
 export default async function init(component, props) {
