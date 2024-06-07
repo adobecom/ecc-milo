20c4ecef6326a1293d8f28260481a64b7e58f51e
Qiyun Dai <pianoquin@gmail.com>
2024-06-07 18:26:01 -0500

    Update event-partners-component-controller.js

--- blocks/form-handler/controllers/event-partners-component-controller.js
+++ blocks/form-handler/controllers/event-partners-component-controller.js
@@ -2,7 +2,7 @@ export function onSubmit(component, props) {
   const partnersGroup = component.querySelector('partner-selector-group');
   const partnerVisible = component.querySelector('#partners-visible')?.checked;
 
-  const partners = partnersGroup.getSelectedPartners();
+  const partners = partnersGroup?.getSelectedPartners();
 
   props.payload = { ...props.payload, partners, partnerVisible };
 }
