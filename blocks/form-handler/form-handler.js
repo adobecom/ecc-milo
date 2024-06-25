acd02a87fc8011be485063f60a94a23b2d1af364
Qiyun Dai <pianoquin@gmail.com>
2024-06-24 21:01:04 -0500

    save on sidenav navigation too

--- blocks/form-handler/form-handler.js
+++ blocks/form-handler/form-handler.js
@@ -489,8 +489,9 @@ function initNavigation(props) {
   });
 
   navItems.forEach((nav, i) => {
-    nav.addEventListener('click', () => {
+    nav.addEventListener('click', async () => {
       if (!nav.disabled) {
+        await saveEvent(props);
         navigateForm(props, i);
       }
     });
