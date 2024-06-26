e1392ac83366d0d6cb9c6392b9eea1fefb26fe7e
Qiyun Dai <pianoquin@gmail.com>
2024-06-26 16:03:22 -0500

    Update product-selector.js

--- ecc/components/product-selector/product-selector.js
+++ ecc/components/product-selector/product-selector.js
@@ -62,7 +62,7 @@ export default class ProductSelector extends LitElement {
   render() {
     return html`
       <fieldset class="rsvp-field-wrapper">
-      ${html`<img class="product-img" src="${this.selectedProduct.tagImage || '/icons/icon-placeholder.svg'}" alt="${this.selectedProduct.title || nothing}">`}  
+      ${html`<img class="product-img" src="${this.selectedProduct.tagImage || '/ecc/icons/icon-placeholder.svg'}" alt="${this.selectedProduct.title || nothing}">`}  
         <sp-picker class="product-select-input" label="Select a product" value=${this.selectedProduct.name || nothing} @change="${this.handleSelectChange}">
           ${this.products.map((product) => html`<sp-menu-item value="${product.name}">${product.title}
           </sp-menu-item>`)}
