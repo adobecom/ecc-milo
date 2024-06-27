04a88f7565e3a9ea5d776e2de1e4e5fc306886e7
Qiyun Dai <pianoquin@gmail.com>
2024-06-27 09:23:02 -0500

    retire lib getter and setter

--- ecc/scripts/utils.js
+++ ecc/scripts/utils.js
@@ -1,3 +1,5 @@
+
+
 /*
  * Copyright 2022 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
@@ -10,6 +12,8 @@
  * governing permissions and limitations under the License.
  */
 
+import { LIBS } from "./scripts";
+
 /**
  * The decision engine for where to get Milo's libs from.
  */
@@ -20,22 +24,6 @@
  * Note: This file should have no self-invoking functions.
  * ------------------------------------------------------------
  */
-
-export const [setLibs, getLibs] = (() => {
-  let libs;
-  return [
-    (prodLibs, location) => {
-      libs = (() => {
-        const { hostname, search } = location || window.location;
-        if (!(hostname.includes('.hlx.') || hostname.includes('local'))) return prodLibs;
-        const branch = new URLSearchParams(search).get('milolibs') || 'ecc';
-        if (branch === 'local') return 'http://localhost:6456/libs';
-        return branch.includes('--') ? `https://${branch}.hlx.live/libs` : `https://${branch}--milo--adobecom.hlx.live/libs`;
-      })();
-      return libs;
-    }, () => libs,
-  ];
-})();
 
 export function decorateArea(area = document) {
   const eagerLoad = (parent, selector) => {
@@ -58,5 +46,5 @@ export function decorateArea(area = document) {
 }
 
 export async function importMiloUtils() {
-  return import(`${getLibs()}/utils/utils.js`);
+  return import(`${LIBS}/utils/utils.js`);
 }
