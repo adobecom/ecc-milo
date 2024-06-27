4d32ec07f58cdb0ef8c5302eff61e16301d6564f
Qiyun Dai <pianoquin@gmail.com>
2024-06-27 17:02:44 -0500

    Update product-selector-group.js

--- ecc/components/product-selector-group/product-selector-group.js
+++ ecc/components/product-selector-group/product-selector-group.js
@@ -18,6 +18,7 @@ export default class ProductSelectorGroup extends LitElement {
 
   constructor() {
     super();
+    this.selectedProducts = this.selectedProducts?.length || [defaultProductValue];
 
     try {
       this.products = JSON.parse(this.dataset.products);
@@ -80,7 +81,6 @@ export default class ProductSelectorGroup extends LitElement {
   render() {
     this.products = this.dataset.products ? JSON.parse(this.dataset.products) : [];
     this.selectedTopics = this.dataset.selectedTopics ? JSON.parse(this.dataset.selectedTopics) : [];
-    this.selectedProducts = this.selectedProducts?.length || [defaultProductValue];
     const uniqueProducts = this.getUniqueProducts();
 
     if (uniqueProducts.length === 0) return html`<div class="error">No product available for topics selected</div>`;
