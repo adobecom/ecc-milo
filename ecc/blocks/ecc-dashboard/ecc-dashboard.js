907a79ae26e8cfbf447bb58e5e5d6def0b8c4440
Qiyun Dai <pianoquin@gmail.com>
2024-06-28 15:16:18 -0500

    Update ecc-dashboard.js

--- ecc/blocks/ecc-dashboard/ecc-dashboard.js
+++ ecc/blocks/ecc-dashboard/ecc-dashboard.js
@@ -291,7 +291,7 @@ async function populateRow(props, config, index) {
 
   initMoreOptions(props, config, event, moreOptionsCell);
 
-  if (event.eventId === sp.get('newEventId') && !props.toasted) {
+  if (event.eventId === sp.get('newEventId') && !props.noop.toasted) {
     const msgTemplate = config['new-event-toast-msg'] instanceof Array ? config['new-event-toast-msg'].join('<br/>') : config['new-event-toast-msg'];
     const toastMsg = buildToastMsg(event.title, msgTemplate);
     createTag('sp-toast', { open: true, variant: 'positive' }, toastMsg, { parent: toastArea });
@@ -299,7 +299,7 @@ async function populateRow(props, config, index) {
     props.noop = { ...props.noop, toasted: true };
   }
 
-  if (event.eventId === sp.get('clonedEventId') && !props.toasted) {
+  if (event.eventId === sp.get('clonedEventId') && !props.noop.toasted) {
     const msgTemplate = config['clone-event-toast-msg'] instanceof Array ? config['clone-event-toast-msg'].join('<br/>') : config['clone-event-toast-msg'];
     const toastMsg = buildToastMsg(event.title, msgTemplate);
     createTag('sp-toast', { open: true, variant: 'positive' }, toastMsg, { parent: toastArea });
