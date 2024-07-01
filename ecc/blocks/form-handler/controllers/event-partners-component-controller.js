30d51ff30d0f28326f56ec4903a33f36e5592e84
Qiyun Dai <pianoquin@gmail.com>
2024-07-01 15:17:14 -0500

    Update event-partners-component-controller.js

--- ecc/blocks/form-handler/controllers/event-partners-component-controller.js
+++ ecc/blocks/form-handler/controllers/event-partners-component-controller.js
@@ -1,8 +1,23 @@
+import { addSponsorToEvent } from '../../../utils/esp-controller.js';
+import { getFilteredCachedPayload } from '../data-handler.js';
+
 /* eslint-disable no-unused-vars */
 export function onSubmit(component, props) {
   if (component.closest('.fragment')?.classList.contains('hidden')) return;
 
   const partnerVisible = component.querySelector('#partners-visible')?.checked;
+  const partnerSelectorGroup = component.querySelector('partner-selector-group');
+
+  if (partnerSelectorGroup) {
+    const partners = partnerSelectorGroup.getSavedPartners();
+    partners.forEach((partner) => {
+      const { sponsorId, sponsorType } = partner;
+      addSponsorToEvent({
+        sponsorId,
+        sponsorType,
+      }, getFilteredCachedPayload().eventId);
+    });
+  }
 
   props.payload = { ...props.payload, partnerVisible };
 }
