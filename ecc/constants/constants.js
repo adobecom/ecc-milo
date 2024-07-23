82feba43004bd64847e61b4f2063e03680d4dcaf
Qiyun Dai <pianoquin@gmail.com>
2024-07-22 19:30:33 -0500

    Update constants.js

--- ecc/constants/constants.js
+++ ecc/constants/constants.js
@@ -1,1 +1,4 @@
-export const LINK_REGEX = '^https:\\/\\/[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$';
+/* eslint-disable no-useless-escape */
+/* eslint-disable import/prefer-default-export */
+
+export const LINK_REGEX = '^https:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?(\?.*)?(#.*)?$';
