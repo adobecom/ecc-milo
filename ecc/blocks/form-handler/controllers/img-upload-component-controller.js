0a32be0629a3d9562e5d63a6e295b54fa2648da8
Qiyun Dai <pianoquin@gmail.com>
2024-07-23 18:12:46 -0500

    Update img-upload-component-controller.js

--- ecc/blocks/form-handler/controllers/img-upload-component-controller.js
+++ ecc/blocks/form-handler/controllers/img-upload-component-controller.js
@@ -117,10 +117,10 @@ export default async function init(component, props) {
           dialogDeleteBtn.disabled = true;
           dialogCancelBtn.disabled = true;
           const resp = await deleteImage(configs, imageId);
-          if (!(resp.errors || resp.message)) {
-            dz.deleteImage();
-          } else {
+          if (resp.errors || resp.message) {
             dz.dispatchEvent(new CustomEvent('show-error-toast', { detail: { message: 'Failed to delete the image. Please try again later.' }, bubbles: true, composed: true }));
+          } else {
+            dz.deleteImage();
           }
         } catch (error) {
           dz.dispatchEvent(new CustomEvent('show-error-toast', { detail: { message: 'Failed to delete the image. Please try again later.' }, bubbles: true, composed: true }));
