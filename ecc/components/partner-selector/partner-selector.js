b46563ea0859699c00f6cbd385521d4771a44f4f
Qiyun Dai <pianoquin@gmail.com>
2024-07-24 13:01:26 -0500

    Update partner-selector.js

--- ecc/components/partner-selector/partner-selector.js
+++ ecc/components/partner-selector/partner-selector.js
@@ -140,7 +140,7 @@ export default class PartnerSelector extends LitElement {
   this.updateValue('name', event.target.value);
   this.filterSeriesPartners(event.target.value);
 }}></sp-textfield>
-              <sp-overlay trigger="partner-name-input@change">
+              <sp-overlay .trigger="partner-name-input@change" .placement="bottom">
                 ${repeat(this.seriesPartners, (partner) => html`
                   <sp-menu-item @click=${() => {
     this.updateValue('name', partner.name);
