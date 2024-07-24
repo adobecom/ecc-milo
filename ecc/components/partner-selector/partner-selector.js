29c767f0008b16336da70b87bb98f37f292781e9
Qiyun Dai <pianoquin@gmail.com>
2024-07-24 17:10:58 -0500

    Typo

--- ecc/components/partner-selector/partner-selector.js
+++ ecc/components/partner-selector/partner-selector.js
@@ -154,7 +154,7 @@ export default class PartnerSelector extends LitElement {
           <div>
             <div class="partner-input">
               <label>${this.fieldLabels.nameLabelText}</label>
-              <custom-search searchKeys=${JSON.stringify([{ searchKey: 'name', renderKey: 'name' }])} data=${JSON.stringify(nameFieldData)} config=${JSON.stringify({})} @change-custom2=${(event) => {
+              <custom-search searchMap=${JSON.stringify([{ searchKey: 'name', renderKey: 'name' }])} data=${JSON.stringify(nameFieldData)} config=${JSON.stringify({})} @change-custom2=${(event) => {
   this.updateValue('name', event.target.value);
 }} @entry-selected=${this.handleAutocomplete} searchdata=${JSON.stringify(this.seriesPartners)} identifier='sponsorId'></custom-search>
             </div>
