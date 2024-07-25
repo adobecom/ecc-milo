a4f89f355cff5e480b0cd419dfd1c73b9cbed87f
Qiyun Dai <pianoquin@gmail.com>
2024-07-25 09:37:23 -0500

    typo

--- ecc/components/partner-selector/partner-selector.js
+++ ecc/components/partner-selector/partner-selector.js
@@ -31,7 +31,7 @@ export default class PartnerSelector extends LitElement {
       placeholder: 'Enter partner name',
     };
 
-    const searchMap = { searchKey: ['name'], renderKey: ['name'] };
+    const searchMap = { searchKeys: ['name'], renderKeys: ['name'] };
     return { searchMap, nameFieldData };
   }
 
