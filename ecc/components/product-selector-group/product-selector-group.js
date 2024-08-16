3ee5387cb1d223a0ba0a60f0f2217cf99038a3ef
Qiyun Dai <pianoquin@gmail.com>
2024-08-16 15:13:26 -0500

    switch icon for clearing

--- ecc/components/product-selector-group/product-selector-group.js
+++ ecc/components/product-selector-group/product-selector-group.js
@@ -98,7 +98,7 @@ export default class ProductSelectorGroup extends LitElement {
           @update-product=${(event) => this.handleProductUpdate(event, index)}>
           <div slot="delete-btn" class="delete-btn">
             ${this.selectedProducts.length === 1 && this.hasOnlyEmptyProductLeft() ? nothing : html`
-              <img class="icon icon-remove-circle" src="/ecc/icons/remove-circle.svg" alt="remove-repeater" @click=${() => this.deleteProduct(index)}></img>
+              <img class="icon icon-remove-circle" src="${this.selectedProducts.length === 1 ? '/ecc/icons/delete.svg' : '/ecc/icons/remove-circle.svg'}" alt="remove-repeater" @click=${() => this.deleteProduct(index)}></img>
             `}
           </div>
         </product-selector>
