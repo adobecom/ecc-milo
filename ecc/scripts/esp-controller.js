6f4e7c244fdf56dfc40012284ddc9d753b14993b
Qiyun Dai <pianoquin@gmail.com>
2024-09-25 10:25:31 -0500

    update Event to ESL

--- ecc/scripts/esp-controller.js
+++ ecc/scripts/esp-controller.js
@@ -531,7 +531,7 @@ export async function updateSpeaker(profile, seriesId) {
 }
 
 export async function updateEvent(eventId, payload) {
-  const { host } = getAPIConfig().esp[ECC_ENV];
+  const { host } = getAPIConfig().esl[ECC_ENV];
   const raw = JSON.stringify({ ...payload, liveUpdate: false });
   const options = await constructRequestOptions('PUT', raw);
 
@@ -552,7 +552,7 @@ export async function updateEvent(eventId, payload) {
 }
 
 export async function publishEvent(eventId, payload) {
-  const { host } = getAPIConfig().esp[ECC_ENV];
+  const { host } = getAPIConfig().esl[ECC_ENV];
   const raw = JSON.stringify({ ...payload, published: true, liveUpdate: true });
   const options = await constructRequestOptions('PUT', raw);
 
@@ -573,7 +573,7 @@ export async function publishEvent(eventId, payload) {
 }
 
 export async function unpublishEvent(eventId, payload) {
-  const { host } = getAPIConfig().esp[ECC_ENV];
+  const { host } = getAPIConfig().esl[ECC_ENV];
   const raw = JSON.stringify({ ...payload, published: false, liveUpdate: true });
   const options = await constructRequestOptions('PUT', raw);
 
