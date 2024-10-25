ddab0b95b60ca527675550cddb3c294dd5f6ecf9
Qiyun Dai <pianoquin@gmail.com>
2024-10-25 13:18:53 -0500

    Update attendee-management-table.js

--- ecc/blocks/attendee-management-table/attendee-management-table.js
+++ ecc/blocks/attendee-management-table/attendee-management-table.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import { getAllEventAttendees, getEvents } from '../../scripts/esp-controller.js';
+import { getAllEventAttendees, getEventsForUser } from '../../scripts/esp-controller.js';
 import { LIBS } from '../../scripts/scripts.js';
 import {
   getIcon,
@@ -11,7 +11,7 @@ import {
 } from '../../scripts/utils.js';
 import { SearchablePicker } from '../../components/searchable-picker/searchable-picker.js';
 import { FilterMenu } from '../../components/filter-menu/filter-menu.js';
-import { initProfileLogicTree } from '../../scripts/profile.js';
+import { getUser, initProfileLogicTree, userHasAccessToEvent } from '../../scripts/profile.js';
 
 const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
@@ -509,16 +509,6 @@ function buildDashboardTable(props, config) {
   populateTable(props, config);
 }
 
-async function getEventsArray() {
-  const resp = await getEvents();
-
-  if (resp.error) {
-    return [];
-  }
-
-  return resp.events;
-}
-
 function renderTableLoadingOverlay(props) {
   const tableContainer = props.el.querySelector('.dashboard-table-container');
   const loadingOverlay = createTag('div', { class: 'loading-overlay' });
@@ -625,7 +615,7 @@ async function buildDashboard(el, config) {
   createTag('div', { class: 'dashboard-body-container' }, '', { parent: mainContainer });
 
   const uspEventId = new URLSearchParams(window.location.search).get('eventId');
-  const events = await getEventsArray();
+  const events = await getEventsForUser();
 
   const props = {
     el,
@@ -641,8 +631,13 @@ async function buildDashboard(el, config) {
   let data = [];
 
   if (props.currentEventId) {
-    const resp = await getAllEventAttendees(props.currentEventId);
-    if (resp && !resp.error) data = resp;
+    if (userHasAccessToEvent(await getUser(), props.currentEventId)) {
+      const resp = await getAllEventAttendees(props.currentEventId);
+      if (resp && !resp.error) data = resp;
+    } else {
+      buildNoAccessScreen(el);
+      return;
+    }
   }
 
   props.data = data;
