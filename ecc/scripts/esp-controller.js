5f774c4c63b48fdf75320a22b1473e07a283c123
Qiyun Dai <pianoquin@gmail.com>
2024-11-08 13:25:08 -0600

    updating Oregon temporary endpoints

--- ecc/scripts/esp-controller.js
+++ ecc/scripts/esp-controller.js
@@ -5,15 +5,15 @@ const API_CONFIG = {
   esl: {
     dev: { host: 'https://wcms-events-service-layer-deploy-ethos102-stage-va-9c3ecd.stage.cloud.adobe.io' },
     dev02: { host: 'https://wcms-events-service-layer-deploy-ethos102-stage-va-d5dc93.stage.cloud.adobe.io' },
-    stage: { host: 'https://events-service-layer-stage.adobe.io' },
-    stage02: { host: 'https://events-service-layer-stage02.adobe.io' },
+    stage: { host: 'https://events-service-layer-stage-or2.adobe.io' },
+    stage02: { host: 'https://events-service-layer-stage02-or2.adobe.io' },
     prod: { host: 'https://events-service-layer.adobe.io' },
   },
   esp: {
     dev: { host: 'https://wcms-events-service-platform-deploy-ethos102-stage-caff5f.stage.cloud.adobe.io' },
     dev02: { host: 'https://wcms-events-service-platform-deploy-ethos102-stage-c81eb6.stage.cloud.adobe.io' },
-    stage: { host: 'https://events-service-platform-stage.adobe.io' },
-    stage02: { host: 'https://events-service-platform-stage02.adobe.io' },
+    stage: { host: 'https://events-service-platform-stage-or2.adobe.io' },
+    stage02: { host: 'https://events-service-platform-stage02-or2.adobe.io' },
     prod: { host: 'https://events-service-platform.adobe.io' },
   },
 };
