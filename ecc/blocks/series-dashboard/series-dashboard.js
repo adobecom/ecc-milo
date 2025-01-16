f72acb62f5e0929604909d2f191d1693fdce4cd3
Qiyun Dai <pianoquin@gmail.com>
2025-01-16 16:35:09 -0500

    Add seriesObj in the archive call

--- ecc/blocks/series-dashboard/series-dashboard.js
+++ ecc/blocks/series-dashboard/series-dashboard.js
@@ -229,7 +229,7 @@ function initMoreOptions(props, config, seriesObj, row) {
           underlay.open = false;
           dialog.innerHTML = '';
           row.classList.add('pending');
-          const resp = await archiveSeries(seriesObj.seriesId);
+          const resp = await archiveSeries(seriesObj.seriesId, seriesObj);
 
           if (resp.error) {
             row.classList.remove('pending');
