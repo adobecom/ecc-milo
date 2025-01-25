bd05215f1b0428972009f9e797a306893428a91b
Qiyun Dai <pianoquin@gmail.com>
2025-01-24 21:48:20 -0500

    Fixing agendaTime update issue

--- ecc/components/agenda-fieldset-group/agenda-fieldset-group.js
+++ ecc/components/agenda-fieldset-group/agenda-fieldset-group.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import { LIBS } from '../../scripts/scripts.js';
+import { parse24HourFormat } from '../../scripts/utils.js';
 import { style } from './agenda-fieldset-group.css.js';
 
 const { LitElement, html, repeat, nothing } = await import(`${LIBS}/deps/lit-all.min.js`);
@@ -53,8 +54,17 @@ export default class AgendaFieldsetGroup extends LitElement {
 
   render() {
     return html`
-      ${repeat(this.agendaItems, (agendaItem, index) => html`
-        <agenda-fieldset .agendas=${this.agendaItems} .agenda=${agendaItem} .timeslots=${this.timeslots} .options=${this.options}
+      ${repeat(this.agendaItems, (agendaItem, index) => {
+    const { hours, minutes, period } = agendaItem.startTime ? parse24HourFormat(agendaItem.startTime) : {};
+    const agendaComponents = {
+      startTime: agendaItem.startTime,
+      description: agendaItem.description,
+      startTimeValue: `${hours}:${minutes}`,
+      startTimePeriod: period,
+    };
+
+    return html`
+        <agenda-fieldset .agendas=${this.agendaItems} .agenda=${agendaComponents} .timeslots=${this.timeslots} .options=${this.options}
           @update-agenda=${(event) => this.handleAgendaUpdate(event, index)}>
           <div slot="delete-btn" class="delete-btn">
             ${this.agendaItems.length === 1 && this.hasOnlyEmptyAgendaLeft() ? nothing : html`
@@ -62,7 +72,8 @@ export default class AgendaFieldsetGroup extends LitElement {
             `}
           </div>
         </agenda-fieldset>
-      `)}
+      `;
+  })}
       <repeater-element text="Add agenda time and details" @repeat=${this.addAgenda}></repeater-element>
     `;
   }
