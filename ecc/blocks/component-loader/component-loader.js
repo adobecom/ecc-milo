99a096b42875291e8063af6455f978e36774d7da
Qiyun Dai <pianoquin@gmail.com>
2025-02-06 11:12:43 -0500

    Moved the files in the right place

--- ecc/blocks/component-loader/component-loader.js
+++ ecc/blocks/component-loader/component-loader.js
@@ -1,7 +1,7 @@
-import { getLibs } from '../../scripts/utils.js';
+import { LIBS } from '../../scripts/scripts.js';
 import { RichTextEditor } from '../../components/rich-text-editor/rich-text-editor.js';
 
-const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
 export default async function init(el) {
   customElements.define('rich-text-editor', RichTextEditor);
