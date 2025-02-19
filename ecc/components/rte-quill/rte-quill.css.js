5a489610ddfe19317f1d015414339589f92db8f1
Michael Chan <sin52605@adobe.com>
2025-02-19 15:59:23 -0800

    remove unused files

--- ecc/components/rte-quill/rte-quill.css.js
+++
@@ -1,9 +0,0 @@
-/* stylelint-disable selector-class-pattern */
-import { LIBS } from '../../scripts/scripts.js';
-
-const { css } = await import(`${LIBS}/deps/lit-all.min.js`);
-
-// eslint-disable-next-line import/prefer-default-export
-export const style = css`
-
-`;
