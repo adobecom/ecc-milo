924df6848b132c9aae02749d990b7ba7e16f3362
Qiyun Dai <pianoquin@gmail.com>
2025-04-12 00:58:43 -0400

    Internalize getValue in RTE to validate output

--- ecc/blocks/venue-info-component/controller.js
+++ ecc/blocks/venue-info-component/controller.js
@@ -105,16 +105,7 @@ function getVenueDataInForm(component) {
   const lon = +placeLngInput.value;
   const gmtOffset = +gmtoffsetInput.value;
   const formattedAddress = formattedAddressInput.value;
-
-  let additionalInformation;
-  const additionalInfoHolder = document.createElement('div');
-  additionalInfoHolder.innerHTML = additionalInformationInput?.value;
-
-  if (additionalInfoHolder.textContent.trim() === '') {
-    additionalInformation = '';
-  } else {
-    additionalInformation = additionalInfoHolder.innerHTML;
-  }
+  const additionalInformation = additionalInformationInput.value;
 
   let addressComponents;
 
