e61240b0f0b147e9a7b688808e23f503bf60cade
Qiyun Dai <pianoquin@gmail.com>
2025-06-05 14:21:45 -0400

    new sheets and fields done

--- ecc/blocks/registration-fields-component/controller.js
+++ ecc/blocks/registration-fields-component/controller.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { getAttribute } from '../../scripts/data-utils.js';
 import { setPropsPayload } from '../form-handler/data-handler.js';
-import { EVENT_TYPES, LINK_REGEX } from '../../scripts/constants.js';
+import { EVENT_TYPES, LINK_REGEX, SUPPORTED_CLOUDS } from '../../scripts/constants.js';
+import { LIBS } from '../../scripts/scripts.js';
+
+const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
 export function onSubmit(component, props) {
   if (component.closest('.fragment')?.classList.contains('hidden')) return;
@@ -52,10 +55,15 @@ function setBasicFormAttributes(rsvpForm, eventData, locale) {
 }
 
 function setRsvpFormAttributes(props, eventData, component) {
+  const rsvpFormConfigs = JSON.parse(component.dataset.rsvpFormConfigs);
+  const cloudType = getAttribute(eventData, 'cloudType', props.locale);
   const eventType = getAttribute(eventData, 'eventType', props.locale);
   const registration = getAttribute(eventData, 'registration', props.locale);
 
   const rsvpForm = component.querySelector('div > rsvp-form');
+  const config = rsvpFormConfigs.find(({ cloudType: cType }) => cType === cloudType);
+
+  rsvpForm.setAttribute('data', JSON.stringify(config?.config?.data));
   rsvpForm.setAttribute('eventType', eventType);
 
   if (eventType === EVENT_TYPES.WEBINAR && registration?.type === 'Marketo') {
@@ -65,15 +73,36 @@ function setRsvpFormAttributes(props, eventData, component) {
   }
 }
 
+function updateDescription(component, props) {
+  const rsvpFormConfigs = JSON.parse(component.dataset.rsvpFormConfigs);
+  const cloudType = getAttribute(props.eventDataResp, 'cloudType', props.locale);
+  const config = rsvpFormConfigs.find(({ cloudType: cType }) => cType === cloudType);
+  const requiredFields = config?.config?.data?.filter(({ Required }) => Required === 'x');
+  console.log('requiredFields', requiredFields);
+
+  const descriptionDiv = component.querySelector(':scope > div:nth-of-type(2)');
+  const existingDescription = descriptionDiv.querySelector('p');
+
+  if (existingDescription) {
+    existingDescription.remove();
+  }
+
+  const requiredFieldsDescription = requiredFields.map(({ Label }) => `<strong>${Label}</strong>`).join(', ').replace(/, ([^,]*)$/, ' and $1');
+  createTag('p', { class: 'description' }, `Note: <strong>${SUPPORTED_CLOUDS.find(({ id }) => id === cloudType)?.name}</strong> required fields include ${requiredFieldsDescription}`, { parent: descriptionDiv });
+}
+
 export async function onPayloadUpdate(component, props) {
+  updateDescription(component, props);
   setRsvpFormAttributes(props, props.payload, component);
 }
 
 export async function onRespUpdate(component, props) {
+  updateDescription(component, props);
   setRsvpFormAttributes(props, props.eventDataResp, component);
 }
 
 export default function init(component, props) {
+  updateDescription(component, props);
   setRsvpFormAttributes(props, props.eventDataResp, component);
 }
 
--- ecc/blocks/registration-fields-component/registration-fields-component.js
+++ ecc/blocks/registration-fields-component/registration-fields-component.js
@@ -1,23 +1,20 @@
 import { LIBS } from '../../scripts/scripts.js';
 import { generateToolTip } from '../../scripts/utils.js';
+import { SUPPORTED_CLOUDS } from '../../scripts/constants.js';
 
 const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
-async function decorateRSVPFields(el) {
+async function decorateRSVPFields(el, rsvpFormConfigs) {
   const row = el.querySelector(':scope > div:last-of-type');
 
   if (!row) return;
 
-  const configSheetLocation = row.querySelector('a')?.href;
-  const config = await fetch(configSheetLocation)
-    .then((resp) => (resp.ok ? resp.json() : null))
-    .catch((err) => window.lana?.log(`Failed to load RSVP fields config: ${err}`));
-
+  el.dataset.rsvpFormConfigs = JSON.stringify(rsvpFormConfigs);
   row.innerHTML = '';
 
   createTag(
     'rsvp-form',
-    { class: 'rsvp-form', data: JSON.stringify(config.data) },
+    { class: 'rsvp-form' },
     '',
     { parent: row },
   );
@@ -25,6 +22,10 @@ async function decorateRSVPFields(el) {
 
 export default async function init(el) {
   el.classList.add('form-component');
+  const rsvpFormConfigs = await Promise.all(SUPPORTED_CLOUDS.map(async ({ id }) => {
+    const config = await fetch(`/ecc/system/rsvp-config-sheets/${id.toLowerCase()}.json`).then((resp) => (resp.ok ? resp.json() : null));
+    return { cloudType: id, config };
+  }));
   generateToolTip(el.querySelector(':scope > div:first-of-type'));
-  await decorateRSVPFields(el);
+  await decorateRSVPFields(el, rsvpFormConfigs);
 }
