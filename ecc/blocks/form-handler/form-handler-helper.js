911861857c7d7d8b7fe03330fa2c1983e13a3a35
Qiyun Dai <pianoquin@gmail.com>
2025-06-10 15:45:57 -0400

    Marketo Integration Block (#515)
    * Marketo Integration Block
    
    * add more disable rules
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Update form-handler-helper.js
    
    * Fixing pagination bug, attendee count bug, required field validation bug
    
    * LOF

--- ecc/blocks/form-handler/form-handler-helper.js
+++ ecc/blocks/form-handler/form-handler-helper.js
@@ -113,6 +113,7 @@ export const VANILLA_COMPONENTS = [
   'registration-fields',
   'secondary-cta',
   'video-content',
+  'marketo-integration',
 ];
 
 async function initVanillaComponents(props) {
@@ -201,40 +202,11 @@ export function getCurrentFragment(props) {
   return currentFrag;
 }
 
-export function updateRequiredFields(props) {
-  const currentFrag = getCurrentFragment(props);
-  props[`required-fields-in-${currentFrag.id}`] = currentFrag.querySelectorAll(INPUT_TYPES.join());
-}
-
-export function navigateForm(props, stepIndex) {
-  const index = stepIndex || stepIndex === 0 ? stepIndex : props.currentStep + 1;
-  const frags = props.el.querySelectorAll('.fragment');
-
-  if (index >= frags.length || index < 0) return;
-
-  props.currentStep = index;
-  props.farthestStep = Math.max(props.farthestStep, index);
-
-  window.scrollTo(0, 0);
-  updateRequiredFields(props);
-}
-
-function initDeepLink(props) {
-  const { hash } = window.location;
-
-  if (hash) {
-    const frags = props.el.querySelectorAll('.fragment');
-
-    const targetFragindex = Array.from(frags).findIndex((frag) => `#${frag.id}` === hash);
-
-    if (targetFragindex && targetFragindex <= props.farthestStep) {
-      navigateForm(props, targetFragindex);
-    }
-  }
-}
-
 function validateRequiredFields(fields) {
-  return fields.length === 0 || Array.from(fields).every((f) => f.value && !f.invalid);
+  const enabledFields = Array.from(fields).filter((f) => !f.disabled);
+
+  return enabledFields.length === 0
+    || enabledFields.every((f) => f.value && !f.invalid);
 }
 
 function onStepValidate(props) {
@@ -260,16 +232,56 @@ function onStepValidate(props) {
   };
 }
 
+export function getUpdatedRequiredFields(props) {
+  const currentFrag = getCurrentFragment(props);
+  const requiredFields = currentFrag.querySelectorAll(INPUT_TYPES.join());
+
+  return requiredFields;
+}
+
 function initRequiredFieldsValidation(props) {
   const currentFrag = getCurrentFragment(props);
 
+  const currentRequiredFields = props[`required-fields-in-${currentFrag.id}`];
   const inputValidationCB = onStepValidate(props);
-  props[`required-fields-in-${currentFrag.id}`].forEach((field) => {
+  currentRequiredFields.forEach((field) => {
     field.removeEventListener('change', inputValidationCB);
+  });
+
+  props[`required-fields-in-${currentFrag.id}`] = getUpdatedRequiredFields(props);
+
+  props[`required-fields-in-${currentFrag.id}`].forEach((field) => {
     field.addEventListener('change', inputValidationCB, { bubbles: true });
   });
 
   inputValidationCB();
+}
+
+export function navigateForm(props, stepIndex) {
+  const index = stepIndex || stepIndex === 0 ? stepIndex : props.currentStep + 1;
+  const frags = props.el.querySelectorAll('.fragment');
+
+  if (index >= frags.length || index < 0) return;
+
+  props.currentStep = index;
+  props.farthestStep = Math.max(props.farthestStep, index);
+
+  window.scrollTo(0, 0);
+  initRequiredFieldsValidation(props);
+}
+
+function initDeepLink(props) {
+  const { hash } = window.location;
+
+  if (hash) {
+    const frags = props.el.querySelectorAll('.fragment');
+
+    const targetFragindex = Array.from(frags).findIndex((frag) => `#${frag.id}` === hash);
+
+    if (targetFragindex && targetFragindex <= props.farthestStep) {
+      navigateForm(props, targetFragindex);
+    }
+  }
 }
 
 function enableSideNavForEditFlow(props) {
@@ -286,8 +298,6 @@ function enableSideNavForEditFlow(props) {
       props.farthestStep = Math.max(props.farthestStep, i);
     }
   });
-
-  initRequiredFieldsValidation(props);
 }
 
 async function loadEventData(props) {
@@ -1089,10 +1099,6 @@ export async function buildECCForm(el) {
       const oldValue = target[prop];
       target[prop] = value;
 
-      if (prop.startsWith('required-fields-in-')) {
-        initRequiredFieldsValidation(target);
-      }
-
       switch (prop) {
         case 'currentStep':
         {
@@ -1150,8 +1156,6 @@ export async function buildECCForm(el) {
     },
   };
 
-  const proxyProps = new Proxy(props, dataHandler);
-
   decorateForm(el);
 
   const frags = el.querySelectorAll('.fragment');
@@ -1164,12 +1168,14 @@ export async function buildECCForm(el) {
     });
   });
 
+  const proxyProps = new Proxy(props, dataHandler);
+
   await loadEventData(proxyProps);
   initFormCtas(proxyProps);
   initNavigation(proxyProps);
   await initComponents(proxyProps);
-  updateRequiredFields(proxyProps);
   enableSideNavForEditFlow(proxyProps);
+  initRequiredFieldsValidation(proxyProps);
   initDeepLink(proxyProps);
   updateStatusTag(proxyProps);
   toggleSections(proxyProps);
