16a61f9a98224c0bfd157b4b7529ab11c7427aad
Ambalika Singh <ambsingh@adobe.com>
2025-07-30 22:32:38 +0530

    Adobe connect MCZ changes

--- ecc/blocks/marketo-event-id-component/marketo-event-id-component.js
+++ ecc/blocks/marketo-event-id-component/marketo-event-id-component.js
@@ -5,8 +5,14 @@ const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
 async function decorateMarketoEventIdFields(el) {
   const div = createTag('div', { class: 'marketo-event-id' }, '', { parent: el });
-  createTag('sp-field-label', { size: 'xl', class: 'field-label' }, 'Marketo Event ID', { parent: div });
-  createTag('sp-textfield', { class: 'field-label', placeholder: 'Enter Marketo Event ID', size: 'l' }, '', { parent: div });
+  const isMCZ = createTag('sp-checkbox', { id: 'mcz-event', size: 'xl' }, "You are using the MCZ Program ID", { parent: div });
+  isMCZ.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const textfield = div.querySelector('sp-textfield');
+    (isChecked)?textfield.value = 'mcz':textfield.value = '';
+  });
+  createTag('sp-field-label', { size: 'xl', class: 'field-label' }, 'Adobe Connect MCZ Program ID', { parent: div });
+  createTag('sp-textfield', { class: 'field-label', placeholder: 'Enter Adobe Connect MCZ Program ID', size: 'l', disabled: true }, '', { parent: div });
 }
 
 export default async function init(el) {
