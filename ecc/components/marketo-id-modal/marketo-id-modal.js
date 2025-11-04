f724f2a95e2dfec3845d14063646313923b18fbd
Vikrant Agarwal <vikranta@adobe.com>
2025-11-04 14:23:26 +0530

    fixes

--- ecc/components/marketo-id-modal/marketo-id-modal.js
+++ ecc/components/marketo-id-modal/marketo-id-modal.js
@@ -110,7 +110,7 @@ export default class MarketoIdModal extends LitElement {
 
   openModal() {
     this.open = true;
-    this.errorMessage = null;
+    this.errorMessage = '';
     this.isValid = true;
     // Focus the input field when modal opens
     this.updateComplete.then(() => {
